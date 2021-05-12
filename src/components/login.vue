4533f8a3c70b65d0e7f6a0230a6d555235b144ea
satyr <satyrsuu@163.com>
2021-05-12 09:30:39 +0800

    Initial commit

---
+++ src/components/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="login">
+     login module
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style scoped>
+  #login{
+    font-family: 'Avenir',S Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 0px;
+    background-color: #e5b6b4;
+  }
+</style>
